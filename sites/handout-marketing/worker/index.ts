c04ca25caf2dd3004114c6f3fd1e93c2c6cfe67d
Ben S <ben@playmakerml.com>
2026-07-14 10:12:52 -0400

    Redirect marketing provider hostnames

--- sites/handout-marketing/worker/index.ts
+++ sites/handout-marketing/worker/index.ts
@@ -29,6 +29,12 @@ const worker = {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const url = new URL(request.url);
 
+    if (url.hostname.endsWith(".chatgpt.site")) {
+      url.hostname = "www.handout.link";
+      url.protocol = "https:";
+      return Response.redirect(url.toString(), 308);
+    }
+
     if (url.pathname === "/_vinext/image") {
       const allowedWidths = [...DEFAULT_DEVICE_SIZES, ...DEFAULT_IMAGE_SIZES];
       return handleImageOptimization(request, {
